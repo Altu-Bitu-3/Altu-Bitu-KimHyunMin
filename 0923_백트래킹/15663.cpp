74edabcb9845b9989ae84242a56927ab336b69b6
hyunmin0809 <khm010809@naver.com>
2022-09-29 11:09:49 +0900

    0923 백트래킹

---
+++ 0923_백트래킹/15663.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int SIZE = 8;
+int n, m;
+vector<int> num(SIZE);
+vector<bool> check(SIZE);
+//재귀함수 사용할 것이므로 전역변수로 넣어줬다...
+
+int result[SIZE];
+// vector로 하려 했는데 그렇게 하면 오류가 뜹니다...
+// result.insert(cnt,) num[i]) 여기에서 오류가 뜨는데 아마도 num[i]가 const가 아니라 생기는 문제 같음.
+
+void backtracking(int cnt) // cnt :수열의 인덱스
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < cnt; i++)
+        {
+            cout << result[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int last = 0; // 0을 입력받을 일은 없다.
+    for (int i = 0; i < num.size(); i++)
+    {
+        if (!check[i] && num[i] != last)
+        {
+            result[cnt] = num[i];
+            last = result[cnt];
+            check[i] = true;
+            backtracking(cnt + 1);
+            check[i] = false;
+        }
+    }
+}
+
+void input() //입력 받고, sort와 unique로 정렬하자 -> 라고 생각했는데 그러면 예제 입력 2 케이스에서 걸림. unique는 사용 X
+{
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        num.insert(num.begin() + i, x);
+    }
+
+    sort(num.begin(), num.end());
+}
+
+int main()
+{
+    cin >> n >> m;
+    input();
+    backtracking(0);
+    return 0;
+}
